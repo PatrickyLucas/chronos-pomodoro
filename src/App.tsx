89114456ecc820ad57e3881ba34dda432a212f8c
Patricky Lucas <patrickylucas@hotmail.com>
2025-10-06 17:27:09 -0300

    Aplicando icone e espaçamento no componente Heading

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import { Heading } from './components/Heading';
 
 import './styles/theme.css';
 import './styles/global.css';
+import { TimerIcon } from 'lucide-react';
 
 export function App() {
   return (
     <>
-      <Heading>Olá mundo</Heading>
+      <Heading>
+        Olá mundo
+        <button>
+          <TimerIcon />
+        </button>
+      </Heading>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
         voluptas nulla aliquid minima odio ducimus quidem aliquam totam,
