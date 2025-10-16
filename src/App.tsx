e4b088836936a7a99cc7d16be7cd3f48795ab342
Patricky Lucas <patrickylucas@hotmail.com>
2025-10-16 12:24:06 -0300

    Criando contexto com creatContext + Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 import { Home } from './pages/Home';
+import type { TaskStateModel } from './models/TaskStateModel';
+import { TaskContext } from './contexts/TaskContext';
+import { useState } from 'react';
 
 import './styles/theme.css';
 import './styles/global.css';
 
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: '00:00',
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
+
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return (
+    <TaskContext.Provider value={{ outraCoisa: 321 }}>
+      <Home />
+    </TaskContext.Provider>
+  );
 }
