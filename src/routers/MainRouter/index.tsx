d3c9da2b892d2582d19d7754023c23e67795797f
Patricky Lucas <patrickylucas@hotmail.com>
2025-10-23 15:36:37 -0300

    feat: Estrutura da página de configurações dos tempos

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -1,9 +1,10 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router';
 import { AboutPomodoro } from '../../pages/AboutPomodoro';
-import { NotFound } from '../../pages/NotFound';
-import { Home } from '../../pages/Home';
-import { useEffect } from 'react';
 import { History } from '../../pages/History';
+import { Home } from '../../pages/Home';
+import { NotFound } from '../../pages/NotFound';
+import { Settings } from '../../pages/Settings';
 
 function ScrollToTop() {
   const { pathname } = useLocation();
@@ -21,6 +22,7 @@ export function MainRouter() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/history/' element={<History />} />
+        <Route path='/settings/' element={<Settings />} />
         <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
